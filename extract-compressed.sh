0b2f4a28a85848f1f3a9e154aae0f66d2e6b0bf0
Daniel Okwufulueze <daniel.okwufulueze@gmail.com>
2020-02-05 01:42:30 +0100

    - add comments - show execution outcome message

--- extract-compressed.sh
+++ extract-compressed.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# declare variables
 sourceDir=;
 destDir=;
 cleanDestDirExtension=;
 
+# help text to be displayed when
+# script is run with the -h option
 helpText=$(cat << EOF
     Extract multiple compressed files within a directory to any chosen directory
     Version: 1.0.0
@@ -16,19 +19,30 @@ helpText=$(cat << EOF
 EOF
 );
 
+# check if the call to this script was followed by additional tokens
 if [ $# -gt 0 ]; then
+
+    # loop through the tokens [options and their values]
     while [ "$1" != "" ]; do
         case "$1" in
+
+            # check if the -s or --sourceDir option was supplied,
+            # shift if it was and read next token as value
             -s | --sourceDir )
                 shift;
                 sourceDir="$1";
             ;;
 
+            # check if the -d or --destDir option was supplied,
+            # shift if it was and read next token as value
             -d | --destDir )
                 shift;
                 destDir="$1";
             ;;
 
+            # check if the -c or --cleanDestDir option was supplied,
+            # shift if it was and read next token as value
+            # or set its value to csv if no next token
             -c | --cleanDestDir )
                 shift;
                 if [ -z "$1" ]; then
@@ -39,36 +53,50 @@ if [ $# -gt 0 ]; then
                 cleanDestDirExtension="$1";
             ;;
 
+            # check if the -h or --help option was supplied,
+            # display the help text if it was
             -h | --help )
                 echo "${helpText}" | less +p;
                 exit;
             ;;
 
+            # show generic instruction if command was badly formed
             * )
                 echo ":::Invalid option $1 supplied. Enter ./extract-compressed -h to see help."
                 exit;
             ;;
         esac
 
+        # move to the next token in the command
         shift;
     done
 fi
 
-echo $cleanDestDirExtension
-
+# check if the destination directory exists
 if [ -e $destDir ]
 then
+
+    # check if the file extension to clear from the destination
+    # directory was supplied in the script invocation
     if [[ ! -z $cleanDestDirExtension ]]
     then
+
+        # remove all files matching the supplied extension
         $(rm $destDir/*.$cleanDestDirExtension);
     fi
 else
+
+    # create the destination directory if it does not already exist
     $(mkdir $destDir);
 fi
 
+# uncompress each compressed file within the
+# source directory into the destination directory
 for gzFile in ${sourceDir}/*.gz
 do
     gzFileName=$(basename $gzFile);
 
-    gzip -dkc < "$gzFile" > $destDir/${gzFileName%%.gz};
+    gzip -dkc < "$gzFile" > $destDir/${gzFileName%%.gz} &&
+    echo ":::Successfully extracted ${gzFileName} into ${destDir}" ||
+    "<<<Failure - Unable to extract ${gzFileName} into ${destDir}";
 done
